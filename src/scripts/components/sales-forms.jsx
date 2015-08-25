eda40dd4ddd63391eaa229074498c61eb9258ba8
Amilcar J. Infante <amilcar.infante@globant.com>
2015-08-25 17:59:23 -0300

    100934176 - Create Sales form view

--- src/scripts/components/sales-forms.jsx
+++ src/scripts/components/sales-forms.jsx
@@ -1,13 +1,35 @@
-var React = require('react/addons');
+var React = require('react/addons'),
+    navigate = require('react-mini-router').navigate;
 
 require('../../styles/sales-forms.css');
+require('gizmo/ss-gizmo.css');
 
 module.exports = React.createClass({
-        render: function() {
+    goToLink: function(){
+        navigate('/default-message');
+    },
+    render: function() {
         return (
-            <div>
-                <p>This is a sales-forms component</p>
+            <div className="sales-forms content">
+                <div className="row">
+                    <div className="options link">
+                        form delivery
+                    </div>
+                </div>
+                <div className="row blank">
+                    <div className="options" 
+                        onClick={this.goToLink}>
+                        <div className="title fl">
+                            <p>Default email message</p>
+                        </div>
+                        <div className="option fr">
+                            <i className="ss-gizmo ss-navigateright"></i>
+                        </div>
+                        <div className="clear"></div>
+                    </div>
+                </div>
             </div>
         );
     }
 });
+
